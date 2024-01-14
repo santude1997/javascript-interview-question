63b4e7c37c062d93ec57a5f7f4da8462439cb22d
santude1997 <santude1997@outlook.com>
2024-01-14 15:06:19 +0530

    duplicate array and integer is added

---
+++ Integer_number.js
@@ -0,0 +1,9 @@
+// find a string is an integer or not
+
+var num = 12.1;
+
+if (num % 1 === 0) {
+  console.log("Integer Number");
+} else {
+  console.log("Not Integer Number");
+}
---
+++ duplicate_array.js
@@ -0,0 +1,7 @@
+// duplicate an array in a single array 
+
+function duplicate(array){
+    return array.concat(array);
+}
+
+console.log(duplicate([1,2,3,4,5]));
